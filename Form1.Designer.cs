23719056f0c58af9d624840f31a0ccdff0053bf5
bruno rius <brunorius3@gmail.com>
2021-10-12 21:58:27 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿
+namespace Colas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.desde = new System.Windows.Forms.TextBox();
+            this.hasta = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.limInferiorAct4 = new System.Windows.Forms.TextBox();
+            this.mediaExpAct5 = new System.Windows.Forms.TextBox();
+            this.limInferiorAct2 = new System.Windows.Forms.TextBox();
+            this.limSuperiorAct2 = new System.Windows.Forms.TextBox();
+            this.limInferiorAct1 = new System.Windows.Forms.TextBox();
+            this.limSuperiorAct1 = new System.Windows.Forms.TextBox();
+            this.mediaExpAct3 = new System.Windows.Forms.TextBox();
+            this.limSuperiorAct4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl23 = new System.Windows.Forms.Label();
+            this.cant = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(623, 175);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(170, 86);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(614, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Desde:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(619, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hasta:";
+            // 
+            // desde
+            // 
+            this.desde.Location = new System.Drawing.Point(688, 47);
+            this.desde.Name = "desde";
+            this.desde.Size = new System.Drawing.Size(100, 23);
+            this.desde.TabIndex = 3;
+            // 
+            // hasta
+            // 
+            this.hasta.Location = new System.Drawing.Point(688, 76);
+            this.hasta.Name = "hasta";
+            this.hasta.Size = new System.Drawing.Size(100, 23);
+            this.hasta.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.limSuperiorAct1);
+            this.groupBox1.Controls.Add(this.limInferiorAct1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(157, 114);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actividad 1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.limSuperiorAct2);
+            this.groupBox2.Controls.Add(this.limInferiorAct2);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(12, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(157, 125);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Actividad 2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.mediaExpAct5);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(339, 75);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(157, 125);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Actividad 5";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.limSuperiorAct4);
+            this.groupBox4.Controls.Add(this.limInferiorAct4);
+            this.groupBox4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox4.Location = new System.Drawing.Point(176, 132);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(157, 125);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Actividad 4";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label9);
+            this.groupBox5.Controls.Add(this.mediaExpAct3);
+            this.groupBox5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox5.Location = new System.Drawing.Point(175, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(158, 114);
+            this.groupBox5.TabIndex = 9;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Actividad 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(6, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "A:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(6, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "B:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(6, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "A:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(6, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "B:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(7, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "B:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(6, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "A:";
+            // 
+            // limInferiorAct4
+            // 
+            this.limInferiorAct4.Location = new System.Drawing.Point(40, 43);
+            this.limInferiorAct4.Name = "limInferiorAct4";
+            this.limInferiorAct4.Size = new System.Drawing.Size(100, 32);
+            this.limInferiorAct4.TabIndex = 14;
+            // 
+            // mediaExpAct5
+            // 
+            this.mediaExpAct5.Location = new System.Drawing.Point(29, 78);
+            this.mediaExpAct5.Name = "mediaExpAct5";
+            this.mediaExpAct5.Size = new System.Drawing.Size(100, 32);
+            this.mediaExpAct5.TabIndex = 15;
+            // 
+            // limInferiorAct2
+            // 
+            this.limInferiorAct2.Location = new System.Drawing.Point(40, 43);
+            this.limInferiorAct2.Name = "limInferiorAct2";
+            this.limInferiorAct2.Size = new System.Drawing.Size(100, 32);
+            this.limInferiorAct2.TabIndex = 13;
+            // 
+            // limSuperiorAct2
+            // 
+            this.limSuperiorAct2.Location = new System.Drawing.Point(39, 82);
+            this.limSuperiorAct2.Name = "limSuperiorAct2";
+            this.limSuperiorAct2.Size = new System.Drawing.Size(100, 32);
+            this.limSuperiorAct2.TabIndex = 14;
+            // 
+            // limInferiorAct1
+            // 
+            this.limInferiorAct1.Location = new System.Drawing.Point(39, 35);
+            this.limInferiorAct1.Name = "limInferiorAct1";
+            this.limInferiorAct1.Size = new System.Drawing.Size(100, 32);
+            this.limInferiorAct1.TabIndex = 12;
+            // 
+            // limSuperiorAct1
+            // 
+            this.limSuperiorAct1.Location = new System.Drawing.Point(39, 73);
+            this.limSuperiorAct1.Name = "limSuperiorAct1";
+            this.limSuperiorAct1.Size = new System.Drawing.Size(100, 32);
+            this.limSuperiorAct1.TabIndex = 13;
+            // 
+            // mediaExpAct3
+            // 
+            this.mediaExpAct3.Location = new System.Drawing.Point(30, 73);
+            this.mediaExpAct3.Name = "mediaExpAct3";
+            this.mediaExpAct3.Size = new System.Drawing.Size(100, 32);
+            this.mediaExpAct3.TabIndex = 4;
+            // 
+            // limSuperiorAct4
+            // 
+            this.limSuperiorAct4.Location = new System.Drawing.Point(40, 82);
+            this.limSuperiorAct4.Name = "limSuperiorAct4";
+            this.limSuperiorAct4.Size = new System.Drawing.Size(100, 32);
+            this.limSuperiorAct4.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(43, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(69, 25);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Media:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(40, 39);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(69, 25);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Media:";
+            // 
+            // lbl23
+            // 
+            this.lbl23.AutoSize = true;
+            this.lbl23.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl23.Location = new System.Drawing.Point(590, 12);
+            this.lbl23.Name = "lbl23";
+            this.lbl23.Size = new System.Drawing.Size(92, 25);
+            this.lbl23.TabIndex = 10;
+            this.lbl23.Text = "Cantidad:";
+            // 
+            // cant
+            // 
+            this.cant.Location = new System.Drawing.Point(688, 14);
+            this.cant.Name = "cant";
+            this.cant.Size = new System.Drawing.Size(100, 23);
+            this.cant.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 271);
+            this.Controls.Add(this.cant);
+            this.Controls.Add(this.lbl23);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.hasta);
+            this.Controls.Add(this.desde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox desde;
+        private System.Windows.Forms.TextBox hasta;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox limSuperiorAct1;
+        private System.Windows.Forms.TextBox limInferiorAct1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox limSuperiorAct2;
+        private System.Windows.Forms.TextBox limInferiorAct2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox mediaExpAct5;
+        private System.Windows.Forms.TextBox limInferiorAct4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox limSuperiorAct4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox mediaExpAct3;
+        private System.Windows.Forms.Label lbl23;
+        private System.Windows.Forms.TextBox cant;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colas
+{
+    public partial class Form1 : Form
+    {
+        double cantidad;
+        double mostrarDesde;
+        double mostrarHasta;
+
+        double limInferiorActividad1;
+        double limInferiorActividad2;
+        double limInferiorActividad4;
+        double limSuperiorActividad1;
+        double limSuperiorActividad2;
+        double limSuperiorActividad4;
+
+        double mediaExponencialActividad3;
+        double mediaExponencialActividad5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void tomarDatos()
+        {
+            cantidad = double.Parse(cant.Text);
+            mostrarDesde = double.Parse(desde.Text);
+            mostrarHasta = double.Parse(hasta.Text);
+
+            limInferiorActividad1 = double.Parse(limInferiorAct1.Text);
+            limInferiorActividad2 = double.Parse(limInferiorAct2.Text);
+            limInferiorActividad4 = double.Parse(limInferiorAct4.Text);
+
+            limSuperiorActividad1 = double.Parse(limSuperiorAct1.Text);
+            limSuperiorActividad2 = double.Parse(limSuperiorAct2.Text);
+            limSuperiorActividad4 = double.Parse(limSuperiorAct4.Text);
+
+            mediaExponencialActividad3 = double.Parse(mediaExpAct3.Text);
+            mediaExponencialActividad5 = double.Parse(mediaExpAct5.Text);
+        }
+    }
+}
